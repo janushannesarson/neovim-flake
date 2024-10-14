cf9e7fa7f3f06e227e82b8f7f42015a086b2ab6c
janushannesarson <janushannesarson@gmail.com>
2024-10-14 23:37:04 +0200

    added gruvbox and rust-analyzer

--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,16 @@
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
 
+   colorschemes.gruvbox.enable = true;
+
+   plugins.lsp = {
+     enable = true;
+
+     servers = {
+	rust_analyzer.enable = true;
+     };
+   }
+
    opts = {
       number = true;
       relativenumber = true;
