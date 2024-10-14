2d56c77d2f778a8b853ba5fa7439d2d16b12d478
janushannesarson <janushannesarson@gmail.com>
2024-10-14 21:35:08 +0200

    added line numbers

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,10 @@
 {
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
+
+   opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 4;
+    };
 }
